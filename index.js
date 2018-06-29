824851f878cd059c1e0251872eda136f4dfdabba
Sean Lin <seanlin1016@gmail.com>
2018-06-29 11:36:00 +0800

    Comparison/Logical Query Operators

--- index.js
+++ index.js
@@ -30,8 +30,22 @@ async function createCourse(){
 //createCourse()
 
 async function getCourse(){
+    //eq (equal)
+    //ne (not equal)
+    //gt (greater than)
+    //gte (greater than or equal to)
+    //lt (less than)
+    //lte (less than or eqaul to)
+    //in
+    //nin (not in)
+
     const courses = await Course
-        .find({ author: 'Mosh', isPublished: true})
+        // .find({ author: 'Mosh', isPublished: true })
+        // .find({ price: { $gt: 10, $lt: 20 } })  // 10 < price < 20
+        // .find({ price: { $in: [10, 15, 20] } }) // price = 10 or 15 or 20
+        .find()
+        .or([ { author: 'Mosh'}, {isPublished: true} ])
+        .and([ { author: 'Mosh'}, {isPublished: true} ])
         .limit(10)
         .sort({ name: 1})  //1 indicates ascending order; -1 indicates descending order
         .select({ name: 1, tags: 1})
